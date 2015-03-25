f5609f08e08115a28d0b52197af80af464d39c9e
Hendrik Choy <hchoy21@gmail.com>
2015-03-25 07:13:10 +0000

    static pages created

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+      
+  def full_title(page_title = '')
+    base_title = "Your Best Local Tampa Deliery Service"
+     
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
