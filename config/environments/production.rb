2c5899d7bd587dfbc32e39cdcd2ea23ae40e8314
Hendrik Choy <hchoy21@gmail.com>
2015-04-06 01:58:19 +0000

    finished sign up

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  # force all access to deliver-ez over SSL
+  config.force_ssl = true
+  
 end
